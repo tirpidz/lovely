83ef185dc98048c1485c5849b26c53bf34c2f249
Martin Turcotte <tirpidz@gmail.com>
2021-01-10 21:20:05 -0500

    build(docker): Dockerfile & cmake install target

--- lovely/app/src/main.cpp
+++ lovely/app/src/main.cpp
@@ -2,14 +2,14 @@
 
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     std::cout << "name: " << lovely::LovelyName << std::endl;
     std::cout << "version: " << lovely::LovelyVersion << std::endl;
     std::cout << "major: " << lovely::LovelyMajorVersion << std::endl;
     std::cout << "minor: " << lovely::LovelyMinorVersion << std::endl;
     std::cout << "patch: " << lovely::LovelyPatchVersion << std::endl;
     std::cout << "build timestamp: " << lovely::LovelyBuildTimestamp << std::endl;
-    std::cout << "major: " << lovely::LovelyTag << std::endl;
-    std::cout << "major: " << lovely::LovelyCommit << std::endl;
+    std::cout << "commit: " << lovely::LovelyCommit << std::endl;
     return 0;
 }
