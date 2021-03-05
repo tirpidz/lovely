d3ae77ffb201557eb812302880758410f89c184b
Martin Turcotte <tirpidz@gmail.com>
2021-03-04 20:10:53 -0500

    feat(controller): feeding model to the controller

--- lovely/controller/src/controller.tests.cpp
+++ lovely/controller/src/controller.tests.cpp
@@ -1,11 +1,50 @@
 #include <lovely/controller/controller.h>
+#include <lovely/model/model.h>
+#include <lovely/model/registry.h>
+#include <lovely/model/symbol/etf.h>
+#include <lovely/model/symbol/stock.h>
 
 #include <catch2/catch.hpp>
 
 using namespace lovely;
 
-TEST_CASE("controller number", "[controller]")
+TEST_CASE("controller initialize", "[controller]")
 {
-    controller controller;
-    REQUIRE(controller.number() == 41);
+    const bool bool_ref = true;
+    const int int_ref = 42;
+
+    auto initialization_callback = [&](registry<stock, etf, bool, int>& data) {
+        data.enroll<stock>({{"tse:td", {}}, {"tse:rbc", {}}});
+        data.enroll<etf>({{"tse:vab", {}}});
+        data.enroll<bool>({{"tse:td", bool_ref}});
+        data.enroll<int>({{"tse:td", int_ref}});
+    };
+
+    model<stock, etf, bool, int> model(initialization_callback);
+    model.initialize();
+
+    auto update_external_callback = [&](lovely::model<stock, etf, bool, int>& model) { (void)model; };
+    auto update_derived_callback = [&](lovely::model<stock, etf, bool, int>& model) { (void)model; };
+
+    controller<stock, etf, bool, int> controller(model, update_external_callback, update_derived_callback);
+
+    SECTION("throw when callback is null")
+    {
+        REQUIRE_THROWS(
+            [&]() { lovely::controller<stock, etf, bool, int> controller(model, nullptr, update_derived_callback); }());
+        REQUIRE_THROWS([&]() {
+            lovely::controller<stock, etf, bool, int> controller(model, update_external_callback, nullptr);
+        }());
+        REQUIRE_THROWS([&]() { lovely::controller<stock, etf, bool, int> controller(model, nullptr, nullptr); }());
+    }
+
+    SECTION("throw when model is not initialized")
+    {
+        lovely::model<stock, etf, bool, int> uninitialized_model(initialization_callback);
+
+        REQUIRE_THROWS([&]() {
+            lovely::controller<stock, etf, bool, int> controller(uninitialized_model, update_external_callback,
+                                                                 update_derived_callback);
+        }());
+    }
 }
--- lovely/model/include/lovely/model/model.h
+++ lovely/model/include/lovely/model/model.h
@@ -12,7 +12,7 @@ class model final {
 public:
     model(std::function<void(registry<types...>&)> initialize_callback)
         : _initialize_callback(initialize_callback)
-        , _has_been_initialized(false)
+        , _is_initialized(false)
     {
         if (_initialize_callback == nullptr) {
             throw exception::model::callback_cannot_be_null();
@@ -27,19 +27,20 @@ public:
 
     void initialize()
     {
-        if (_has_been_initialized) {
+        if (_is_initialized) {
             throw exception::model::already_has_been_initialized();
         }
 
-        _has_been_initialized = true;
+        _is_initialized = true;
         _initialize_callback(_data);
     }
 
+    const bool is_initialized() const { return _is_initialized; }
     const registry<types...>& data() { return _data; }
 
 private:
     std::function<void(registry<types...>&)> _initialize_callback;
-    bool _has_been_initialized;
+    bool _is_initialized;
     registry<types...> _data;
 };
 
