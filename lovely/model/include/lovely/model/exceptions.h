fbfd4c5ab299b7059f94b64f638c07b7d9da4ab8
Martin Turcotte <tirpidz@gmail.com>
2021-08-14 14:32:45 -0400

    feat(controller): refactor without callbacks

--- lovely/model/include/lovely/model/exceptions.h
+++ lovely/model/include/lovely/model/exceptions.h
@@ -22,10 +22,6 @@ class already_has_been_initialized final : public std::exception {
     virtual const char* what() const throw() { return "already has been initialized"; }
 };
 
-class callback_cannot_be_null final : public std::exception {
-    virtual const char* what() const throw() { return "callback cannot be null"; }
-};
-
 }  // namespace model
 }  // namespace exception
 }  // namespace lovely
