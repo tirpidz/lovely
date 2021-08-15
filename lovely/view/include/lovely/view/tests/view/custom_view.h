b31b1d76cf9a9e32f34667d528a65e987f795cca
Martin Turcotte <tirpidz@gmail.com>
2021-08-15 08:57:05 -0400

    feat(view): add executors into view

--- lovely/view/include/lovely/view/tests/view/custom_view.h
+++ lovely/view/include/lovely/view/tests/view/custom_view.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <lovely/controller/tests/executor/simple.h>
 #include <lovely/model/tests/model/custom_model.h>
 #include <lovely/view/view.h>
 
@@ -9,8 +10,11 @@ namespace lovely {
 
 class custom_view final : public view<custom_model> {
 public:
-    custom_view(custom_model& m) : view<custom_model>(m) {}
+    custom_view(custom_model& m, simple<custom_model>& simple) : view<custom_model>(m), _simple(simple) {}
     virtual ~custom_view() = default;
+
+protected:
+    simple<custom_model>& _simple;
 };
 
 }  // namespace lovely
