db2e3592ec2f0bf2fccdf58fe421183b8ba80b59
Martin Turcotte <tirpidz@gmail.com>
2021-08-15 09:20:20 -0400

    fix(controller): cleanup

--- lovely/controller/include/lovely/controller/tests/custom_controller.h
+++ lovely/controller/include/lovely/controller/tests/custom_controller.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <lovely/controller/controller.h>
-#include <lovely/controller/exceptions.h>
 #include <lovely/controller/tests/executor/simple.h>
 #include <lovely/controller/tests/updater/custom_updater.h>
 #include <lovely/model/tests/model/custom_model.h>
@@ -10,12 +9,7 @@ namespace lovely {
 
 class custom_controller final : public controller<custom_model, custom_updater, simple<custom_model>> {
 public:
-    custom_controller(custom_model& m) : controller(m)
-    {
-        if (!m.is_initialized()) {
-            throw exception::controller::model_not_initialized();
-        }
-    }
+    custom_controller(custom_model& m) : controller(m) {}
 
     virtual ~custom_controller() = default;
 
